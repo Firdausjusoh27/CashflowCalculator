aeb2196d56d19987c5603bf5fc3b57edc46403c5
Firdausjusoh27 <firdausjusoh27@gmail.com>
2020-12-21 00:30:51 +0800

    Update for main page (Done Main Page)

--- app/src/main/java/com/example/cashflowapp/MainPage.java
+++ app/src/main/java/com/example/cashflowapp/MainPage.java
@@ -3,21 +3,25 @@ package com.example.cashflowapp;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.widget.ProgressBar;
 
 public class MainPage extends AppCompatActivity {
     private ProgressBar cashPB;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        cashPB = findViewById(R.id.progressbar);
         setContentView(R.layout.activity_main_page);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("  CashFlow Apps");
         actionBar.setIcon(R.drawable.cash_2);
         actionBar.setDisplayUseLogoEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
+        cashPB = findViewById(R.id.progressbar);
+        cashPB.setProgress(90);
+
     }
 }
